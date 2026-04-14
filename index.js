7d82b3662ac490ac6e7480a4a46fe6aa8658c272
Shadrack Githu <little.githu@gmail.com>
2026-04-14 18:05:42 +0300

    Completed login tracker with closures and arrow function

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+function createLoginTracker(userInfo) {
+    let attemptCount = 0;
 
+    return (passwordAttempt) => {
+        if (attemptCount >= 3) {
+            return "Account locked due to too many failed login attempts";
+        }
 
+        if (passwordAttempt === userInfo.password) {
+            return "Login successful";
+        }
+
+        attemptCount++;
+
+        if (attemptCount >= 3) {
+            return "Account locked due to too many failed login attempts";
+        }
+
+        return `Attempt ${attemptCount}: Login failed`;
+    };
+}
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+    createLoginTracker
 };
